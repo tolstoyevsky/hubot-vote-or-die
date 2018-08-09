7304c4bfbb1668dd2fa890e1ed650a6f845b2981
Evgeny Golyshev <eugulixes@gmail.com>
2018-08-09 14:28:35 +0300

    Put emojis under polls in advance

--- index.js
+++ index.js
@@ -12,6 +12,7 @@
 //
 
 module.exports = function (robot) {
+    const uuid4 = require('uuidv4');
 
     const route = new RegExp(/^!poll ([^,]+),(.*)$/g);
         
@@ -45,13 +46,14 @@ module.exports = function (robot) {
             if (matches.hasOwnProperty(key)) {
                 const trimmed = matches[key].trim();
                 if (trimmed) {
-                    options.push(emojis[(options.length + 1)] + ' ' + trimmed);
+                    options.push(emojis[(options.length)] + ' ' + trimmed);
                 }
             }
         }
 
         var msg = {};
         msg.msg = '_Please vote using reactions_';
+        msg._id = uuid4();
         msg.attachments = [
                 {
                     color: '#0000DD',
@@ -60,9 +62,27 @@ module.exports = function (robot) {
                 }
             ];
 
-        return res.send(
-            msg
-        );
+        res.send(msg);
 
+        (async function(msgId, emojis, optionsNumber) {
+            const delay = (ms) => {
+                return new Promise(resolve => {
+                    setTimeout(() => {
+                        resolve()
+                    }, ms)
+                })
+            }
+
+            // Wait till the message is in the channel.
+            // TODO: probably there is a better approach how to achieve this.
+            await delay(5000);
+
+            for (let i = 0; i < optionsNumber; i++) {
+                robot.adapter.callMethod('setReaction', emojis[i], msgId);
+
+                await delay(1000);
+            }
+        })(msg._id, emojis, options.length);
     });
 };
+
