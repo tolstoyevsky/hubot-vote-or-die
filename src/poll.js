acf1731c9a49621d30ad585d73353804267d86cb
Evgeny Golyshev <eugulixes@gmail.com>
2018-09-12 16:30:50 +0300

    Meet eslint requirements

--- src/poll.js
+++ src/poll.js
@@ -9,93 +9,90 @@
 //
 
 module.exports = function (robot) {
-    const uuid4 = require('uuidv4');
+  const uuid4 = require('uuidv4')
+  const route = new RegExp(/^!poll ([^,]+),(.*)$/g)
 
-    const route = new RegExp(/^!poll ([^,]+),(.*)$/g);
-        
-    robot.hear(route.source, async (res) => {
+  robot.hear(route.source, async (res) => {
+    const emojis = [
+      ':zero:',
+      ':one:',
+      ':two:',
+      ':three:',
+      ':four:',
+      ':five:',
+      ':six:',
+      ':seven:',
+      ':eight:',
+      ':nine:',
+      ':keycap_ten:',
+      ':keycap_asterisk:'
+    ]
 
-        const emojis = [
-            ':zero:',
-            ':one:',
-            ':two:',
-            ':three:',
-            ':four:',
-            ':five:',
-            ':six:',
-            ':seven:',
-            ':eight:',
-            ':nine:',
-            ':keycap_ten:',
-            ':keycap_asterisk:',
-        ];
+    const title = res.match[1]
+    const matches = res.match[2].split(',')
 
-        const title = res.match[1];
+    let options = []
 
-        let options = [];
+    // The object which represents a poll message.
+    let msg = {}
 
-        // The object which represents a poll message.
-        let msg = {};
+    if (matches.length < 2) {
+      res.send('Provide more than one option.')
+      return
+    }
 
-        matches = res.match[2].split(',');
-        if (matches.length < 2) {
-            res.send('Provide more than one option.');
-            return;
+    if (matches.length > emojis.length) {
+      res.send(`The maximum number of options is limited to ${emojis.length}.`)
+      return
+    }
+
+    for (const match of matches) {
+      const trimmed = match.trim()
+      if (trimmed) {
+        options.push(`${emojis[options.length]} ${trimmed}`)
+      }
+    }
+
+    // Compose and send the poll message (containing the question and options).
+    msg.msg = '_Please vote using reactions_'
+    msg._id = uuid4()
+    msg.attachments = [
+      {
+        color: '#0000DD',
+        title: title,
+        text: options.join('\n')
+      }
+    ]
+
+    res.send(msg)
+
+    const delay = (ms) => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve()
+        }, ms)
+      })
+    }
+
+    const retry = (retries, fn) =>
+      fn().catch(async (err) => {
+        if (retries > 1) {
+          retry(retries - 1, fn)
+
+          await delay(1000)
+        } else {
+          Promise.reject(err)
         }
+      })
 
-        if (matches.length > emojis.length) {
-            res.send(`The maximum number of options is limited to ${emojis.length}.`);
-            return;
-        }
+    // Wait till the message is in the channel.
+    // TODO: probably there is a better approach how to achieve this.
+    await delay(5000)
 
-        for (const match of matches) {
-            const trimmed = match.trim();
-            if (trimmed) {
-                options.push(`${emojis[options.length]} ${trimmed}`);
-            }
-        }
+    for (let i = 0; i < options.length; i++) {
+      retry(3, () => { return robot.adapter.callMethod('setReaction', emojis[i], msg._id) })
 
-        // Compose and send the poll message (containing the question and options).
-        msg.msg = '_Please vote using reactions_';
-        msg._id = uuid4();
-        msg.attachments = [
-            {
-                color: '#0000DD',
-                title: title,
-                text: options.join('\n')
-            }
-        ];
-
-        res.send(msg);
-
-        const delay = (ms) => {
-            return new Promise(resolve => {
-                setTimeout(() => {
-                    resolve()
-                }, ms)
-            })
-        }
-
-        const retry = (retries, fn) =>
-            fn().catch(async (err) => {
-                if (retries > 1) {
-                    retry(retries - 1, fn);
-
-                    await delay(1000);
-                } else {
-                    Promise.reject(err);
-                }
-            });
-
-        // Wait till the message is in the channel.
-        // TODO: probably there is a better approach how to achieve this.
-        await delay(5000);
-
-        for (let i = 0; i < options.length; i++) {
-            retry(3, () => {return robot.adapter.callMethod('setReaction', emojis[i], msg._id)});
-
-            await delay(1000);
-        }
-    });
-};
-
+      await delay(1000)
+    }
+  })
+}
